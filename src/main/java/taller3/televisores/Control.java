b0cb063ac57d5ddb6e6673b434fdcd28663d52a6
alvarojguerrero <guerreroalvarojavier@gmail.com>
2021-03-16 23:00:36 -0500

    Commit
    :/

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,55 +1,45 @@
 package taller3.televisores;
 
 public class Control {
-	TV tv;
-	
-	public void enlazar(TV tv) {
-		this.tv=tv;
-		tv.setControl(this);
-	}
+    TV tv;
 
-	public TV getTv() {
-		return tv;
-	}
-
-	public void setTv(TV tv) {
-		this.tv = tv;
-	}
-	public void turnOn() {
-        tv.estado=true;
+    public void turnOn(){
+        tv.estado = true;
     }
-    
-    public void turnOff() {
-        tv.estado=false;
+    public void turnOff(){
+        tv.estado = false;
     }
-    
-    public void canalUp() {
+    public void canalUp(){
         if (tv.canal < 120 && tv.estado){
             tv.canal ++;
         }
     }
-    
-    public void canalDown() {
-    	if (tv.canal > 1 && tv.estado){
+    public void canalDown(){
+        if (tv.canal > 1 && tv.estado){
             tv.canal --;
         }
     }
-    
-    public void volumenUp() {
-    	if (tv.volumen < 7 && tv.estado){
+    public void setCanal(int canal){
+        if(canal >= 1 && canal <= 120 && tv.estado){
+            tv.canal = canal;
+        }
+    }
+    public void volumenUp(){
+        if (tv.volumen < 7 && tv.estado){
             tv.volumen ++;
         }
     }
-    
-    public void volumenDown() {
-    	if (tv.volumen > 0 && tv.estado){
+    public void volumenDown(){
+        if (tv.volumen > 0 && tv.estado){
             tv.volumen --;
-        }    
-    }
-    
-    public void setCanal(int canal) {
-    	if(tv.estado && (canal >= 1 && canal <= 120) ){
-            tv.canal = canal;
         }
+    }
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public TV getTv() {
+        return tv;
     }
 }
--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -1,17 +1,15 @@
 package taller3.televisores;
 
 public class Marca {
-	String nombre;
-	
-	public Marca(String nombre) {
+    String nombre;
 
-		this.nombre = nombre;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
+    public Marca (String nombre){
+        this.nombre = nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
 }
